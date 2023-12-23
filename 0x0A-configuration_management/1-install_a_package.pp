847aa36359c5aca88d4cae757474c2f73e81f041
Yasir Siddig Sultan <yasir.c.s.ltd.sa@gmail.com>
2023-12-23 16:46:22 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,19 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
+package {'python3.8':
+  ensure   => '3.8.10',
+  }
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+  require => package ['python3-pip']
+}
+package {'python3-pip':
+  ensure   => present,
+  }
+
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip'
   require => package ['python3-pip']
 }
